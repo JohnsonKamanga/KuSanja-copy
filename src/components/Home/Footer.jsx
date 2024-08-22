245c909f7b785146bbbc2531636af982e39c925d
JohnsonKamanga <johnsonkamanga2@gmail.com>
2024-08-22 16:50:11 +0200

    forgot to add these files to the commit about renaming the the HomePage directory to Home

---
+++ src/components/Home/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+
+    return(
+        <div className="p-2 bg-white text-black">
+        <footer>
+            Footer
+        </footer>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+
+function Home(){
+    return(
+        <div className="min-h-screen flex flex-col items-center justify-center font-sora mt-3 p-3">
+            <div className="lg:w-[55%]">
+            <h1 className="text-3xl lg:text-4xl first-line:text-4xl lg:first-line:text-5xl font-bold text-center">Track, Manage, Succeed:<br></br> Your Business Assistant</h1>
+            <p className="text-gray-200 text-center">
+                Welcome to KuSanja, your business assisstant that aims to help you keep track of all your business transactions 
+                by keeping them in a centralized and organized manner.
+            </p>
+        </div>
+        </div>
+    )
+}
+
+export default Home;
