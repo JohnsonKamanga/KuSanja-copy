3730f421b1ae15e9df5f980c0cb12717dc4f1c46
SifisoChitowe <sifisoc2003@gmail.com>
2024-09-28 03:06:18 +0200

    created sign in and sign up pages but still needs some styling

--- src/components/router/routes.jsx
+++ src/components/router/routes.jsx
@@ -4,10 +4,23 @@ import Features from "../Features/Features";
 import FAQ from "../FAQ/FAQ";
 import About from "../About/About";
 import LandingPage from "../LandingPage/LandingPage";
+import SignIn from "../SignIn/SignIn";
+import SignUp from "../SignUp/SignUp";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
         element: <LandingPage/>
     },
+
+    {
+        path: "/SignIn",
+        element: <SignIn/>
+    },
+
+    {
+        path: "/SignUp",
+        element: <SignUp/>
+    },
+
 ])
